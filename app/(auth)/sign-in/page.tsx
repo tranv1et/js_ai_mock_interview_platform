b35ca7f1d485e08ca4ac6470a08ecebf25ce31a1
Tran Viet <riyanz0815@gmail.com>
2025-03-26 21:44:42 +0800

    Auth pages: create auth forms

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import AuthForm from "@/components/AuthForm";
-import React from "react";
 
 const Page = () => {
     return <AuthForm type="sign-in" />;
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,12 +15,15 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
 
 const formSchema = z.object({
     username: z.string().min(2).max(50),
 });
 
-const AuthForm = () => {
+type FormType = "sign-in" | "sign-up";
+const AuthForm = ({ type }: { type: FormType }) => {
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -35,28 +38,42 @@ const AuthForm = () => {
         // ✅ This will be type-safe and validated.
         console.log(values);
     }
+    const isSignIn = type === "sign-in";
+
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </div>
+
+                <h3>Practice job interview with AI</h3>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="w-full space-y-6 mt-4 form"
+                    >
+                        {!isSignIn && <p>Name</p>}
+                        <p>Email</p>
+                        <p>Password</p>
+
+                        <Button className="btn" type="submit">
+                            {isSignIn ? "Sign In" : "Create an Account"}
+                        </Button>
+                    </form>
+                </Form>
+                <p className="text-center">
+                    {isSignIn ? "No account yet?" : "Have an Account already?"}
+                    <Link
+                        href={!isSignIn ? "/sign-in" : "sign-up"}
+                        className="font-bold text-user-primary ml-1"
+                    >
+                        {!isSignIn ? "Sign in" : "Sign up"}
+                    </Link>
+                </p>
+            </div>
+        </div>
     );
 };
 
